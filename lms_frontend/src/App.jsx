275fcc7bf63b023e450f4208f3f3cf7331e5f6ca
Asif294 <asifurrahman677294@gmail.com>
2025-08-14 22:14:41 +0600

    frontend

---
+++ lms_frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='font-bold text-3xl bg-amber-300'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 USER_ROLES =(
     ('admin' ,'admin'),
     ('teacher','Teacher'),
-    ('student',('Student'))
+    ('student','Student')
 )
 
 class User(AbstractUser):
